d70eb0cd194ec7c347d8a844bf284f5ec3729b94
German Escobar <german.escobarc@gmail.com>
2017-08-16 02:00:17 -0500

    Change capistrano configuration

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,8 +1,10 @@
 # config valid only for current version of Capistrano
 lock "3.9.0"
 
+server "34.207.63.248", roles: [:web, :app, :db], primary: true
+
 set :application, "cap-test"
-set :repo_url, "git@example.com:me/my_repo.git"
+set :repo_url, "https://github.com/germanescobar/cap-test.git"
 
 # Default branch is :master
 # ask :branch, `git rev-parse --abbrev-ref HEAD`.chomp
@@ -19,7 +21,7 @@ set :deploy_to, "/app"
 # Default value for :pty is false
 set :pty, true
 
-set :puma_bind,       "unix://#{shared_path}/tmp/sockets/#{fetch(:application)}-puma.sock"
+set :puma_bind,       "tcp://0.0.0.0:9292"
 set :puma_state,      "#{shared_path}/tmp/pids/puma.state"
 set :puma_pid,        "#{shared_path}/tmp/pids/puma.pid"
 set :puma_access_log, "#{release_path}/log/puma.error.log"
@@ -41,3 +43,36 @@ set :puma_preload_app, true
 
 # Default value for keep_releases is 5
 # set :keep_releases, 5
+
+namespace :puma do
+  desc 'Create Directories for Puma Pids and Socket'
+  task :make_dirs do
+    on roles(:app) do
+      execute "mkdir #{shared_path}/tmp/sockets -p"
+      execute "mkdir #{shared_path}/tmp/pids -p"
+    end
+  end
+
+  before :start, :make_dirs
+end
+
+namespace :deploy do
+  desc 'Initial Deploy'
+  task :initial do
+    on roles(:app) do
+      before 'deploy:restart', 'puma:start'
+      invoke 'deploy'
+    end
+  end
+
+  desc 'Restart application'
+  task :restart do
+    on roles(:app), in: :sequence, wait: 5 do
+      invoke 'puma:restart'
+    end
+  end
+
+  after  :finishing,    :compile_assets
+  after  :finishing,    :cleanup
+  after  :finishing,    :restart
+end
